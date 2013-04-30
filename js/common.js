32aaa4b7a2501d8356ee01f36477f10080951a89
Amr Gaber <amr@debrijja.com>
2013-04-29 19:28:59 -0500

    Further updates for navbar

--- js/common.js
+++ js/common.js
@@ -1,86 +1,12 @@
 $( document ).ready(function() {
   /* load navbar */
   $( "#navbar" ).load( "load_navbar.php", function() {
-    $( "#logout" ).click(function() {
+    $( "#logout-button" ).click(function() {
       $.post(
-        'action_logout.php', 
+        'action_logout.php',
         function() { 
           window.location = "login.php";  
-        }
-      );
-    });
-
-    $( "#change-password-menu" ).click(function() {
-      $( "#change-password-modal" ).modal( "show" );
-    });
-
-    var v = $( "#change-password-form" ).validate({
-      rules: {
-        newPassword: {
-          required: true
-        },
-        confirmNewPassword: {
-          required: true,
-          equalTo: "#newPassword"
-        }
-      },
-      messages: {
-        newPassword: {
-          required: "New password is a required field."
-        },
-        confirmNewPassword: {
-          required: "Confirm new password is a required field.",
-          equalTo: "Passwords must match."
-        }
-      },
-      errorPlacement: function( error, element ) {
-        error.appendTo( $( "#change-password-form-status" ) );
-        $( "#change-password-form-status" ).show();
-      },
-      success: function() {
-        if( v.numberOfInvalids() == 0 ) {
-          $( "#change-password-form-status" ).hide();
-          $( "#change-password-form-status" ).html( "" );
-        }
-      }
-    });
-
-    $( "#change-password-form" ).submit(function() {
-      if( !v.form() ) {
-        return false;
-      }
-
-      $( "#change-password-form-status" ).html( "" );
-      $( "#change-password-form-status" ).removeClass( "alert" );
-      $( "#change-password-form-status" ).removeClass( "alert-error" );
-      $( "#change-password-form-status" ).removeClass( "alert-success" );
-
-      $.post(
-        "action_change_password.php",
-        $( "#change-password-form" ).serialize(),
-        function( data, textStatus, jqXHR ) {
-          $( "#change-password-form-status" ).html( jqXHR.responseText );
-          $( "#change-password-form-status" ).show();
-          $( "#change-password-form" ).each(function() {
-            this.reset();
-          });
-        }
-        ).fail(function( data, textStatus, jqXHR ) {
-          $( "#change-password-form-status" ).html( jqXHR.responseText );
-          $( "#change-password-form-status" ).show();
-        }
-        ).always(function( data, textStatus, jqXHR ) {
-          /* Debug */
-          console.log( "Sent     --> " + $( "#change-password-form" ).serialize() );
-          console.log( "Received --> " + jqXHR.responseText );
-        });
-
-      return false;
-    });
-
-    $( "#change-password-modal" ).on( "hidden", function() {
-      $( "#change-password-form-status" ).hide();
-      $( "#change-password-form-status" ).html( "" );
+      });
     });
   });
 
--- load_navbar.php
+++ load_navbar.php
@@ -31,49 +31,13 @@ if( !$_SESSION[ 'username' ] ) {
           <li><a href="search_shop_profile.php">Shop Profiles</a></li>
           <li><a href="search_action.php">Actions</a></li>
         </ul>
-
+        
         <ul class="nav pull-right">
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php echo( $_SESSION[ 'username' ] ); ?> <i class="icon-chevron-down icon-white"></i></a>
-            <ul class="dropdown-menu" role="menu">
-              <li><a href="#" id="change-password-menu">change password</a></li>
-            </ul>
-          </li>
+          <li class="navbar-text"><?php echo( $_SESSION[ 'username' ] ); ?></li>
           <li class="divider-vertical"></li>
-          <li><button type="button" id="logout" class="btn btn-small btn-inverse">Logout</button></li>
+          <li><button type="button" id="logout-button" class="btn btn-small btn-inverse">Logout</button></li>
         </ul>
       </div>
     </div>
   </div>
-</div>
-        
-<div id="change-password-modal" class="modal hide fade">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h3>Change Password</h3>
-  </div>
-    
-  <form id="change-password-form" class="form-horizontal">
-    <div class="modal-body">
-        <div class="control-group">
-          <label class="control-label" for="newPassword">New password</label>
-          <div class="controls">
-            <input type="password" id="newPassword" name="newPassword" placeholder="Type new password here">
-          </div>
-        </div>
-
-        <div class="control-group">
-          <label class="control-label" for="confirmNewPassword">Confirm new password</label>
-          <div class="controls">
-            <input type="password" id="confirmNewPassword" name="confirmNewPassword" placeholder="Retype new password here">
-          </div>
-    </div>
-    
-    <div class="modal-footer">
-      <span id="change-password-form-status" class="alert hide pull-left">
-      </span>
-      <button type="submit" id="change-password-button" class="btn btn-primary">Submit</button>
-      <button type="button" class="btn" data-dismiss="modal">Cancel</button>
-    </div>
-  </form>
 </div>
