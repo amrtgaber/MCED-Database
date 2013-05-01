c76f176d7d6c21badde59279071f1cea0ce52193
Amr Gaber <amr@debrijja.com>
2013-05-01 16:16:48 -0500

    File cleanup

--- js/manage_users.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#add-user").click(function() {
-    window.location = "add_user.php";
-  });
-  
-  $("#remove-user").click(function() {
-    window.location = "remove_user.php";
-  });
-});
