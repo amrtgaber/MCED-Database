55c9db390d6906f04b55804264d26c5eb4fd6de4
Amr Gaber <amr@debrijja.com>
2013-05-08 14:36:50 -0500

    Removed delete contact privilege check

--- action_delete_contact.php
+++ action_delete_contact.php
@@ -16,11 +16,6 @@ if( !$_SESSION[ 'username' ] ) {
   alert_error( "You must be logged in to delete a contact." );
 }
 
-/* Must have privilege level of 3 or greater to access this page */
-if( $_SESSION[ 'privilege_level' ] < 3 ) {
-  alert_error( "You do not have the required privilege level to delete a contact." );
-}
-
 /* get id */
 $id = mysql_real_escape_string( $_POST[ 'id' ] );
 
